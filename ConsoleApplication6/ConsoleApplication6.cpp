03e7b1b6255adce1322f9e7ace4b1d9cf94ba3c0
lolololololol931 <kirill2008tuy@gmail.com>
2025-01-16 22:12:21 +0200

    n1

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -1,20 +1,71 @@
-﻿// ConsoleApplication6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <vector>
+using namespace std;
 
-#include <iostream>
+int** createArray(int rows, int cols) {
+    int** arr = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        arr[i] = new int[cols];
+        for (int j = 0; j < cols; j++) {
+            arr[i][j] = rand() % 21 - 10;
+        }
+    }
+    return arr;
+}
+
+void printArray(int** arr, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+//n1
+void addColumn(int**& arr, int& rows, int& cols, int pos) {
+    if (pos < 0 || pos > cols) {
+        cout << "некоректна позиція " << endl;
+        return;
+    }
+    for (int i = 0; i < rows; i++) {
+        int* newRow = new int[cols + 1]; 
+        for (int j = 0; j < pos; j++) {
+            newRow[j] = arr[i][j];
+        }
+        newRow[pos] = rand() % 21 - 10;
+        for (int j = pos; j < cols; j++) {
+            newRow[j + 1] = arr[i][j];
+        }
+        delete[] arr[i]; 
+        arr[i] = newRow; 
+    }
+    cols++; 
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    system("chcp 1251>null");
+    srand(time(0));
+    int rowsA, colsA, rowsB, colsB, rowsC, colsC,num;
+    cout << "кількість рядків і стовпців для A: ";
+    cin >> rowsA >> colsA;
+    cout << "кількість рядків і стовпців для B: ";
+    cin >> rowsB >> colsB;
+    cout << "кількість рядків і стовпців для C: ";
+    cin >> rowsC >> colsC;
+    int** A = createArray(rowsA, colsA);
+    int** B = createArray(rowsB, colsB);
+    int** C = createArray(rowsC, colsC);
+    cout << "зазначена позиція додавання стовпця: ";
+    cin >> num;
+    addColumn(A, rowsA, colsA, num);
+    cout << "A після додавання стовпця:" << endl;
+    printArray(A, rowsA, colsA);
+
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
